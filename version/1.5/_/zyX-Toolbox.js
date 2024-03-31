eb795a6cf236953cf3b3fe5ab2bb0797a845d694
wumbl3 <wumbl3@gmail.com>
2024-03-31 13:47:05 -0400

    OwO

--- version/1.5/_/zyX-Toolbox.js
+++ version/1.5/_/zyX-Toolbox.js
@@ -1,40 +1,3 @@
-export function raf({ env, func, goal, logging, start = false, callback, after_cycle } = {}) {
-	let state = true;
-	const stop = () => {
-		state = false;
-	},
-		frame = () => {
-			func(env);
-			let go = goal(env);
-			if (!state) return null;
-			if (go) {
-				if (after_cycle) after_cycle(env);
-				if (callback) {
-					let cb = callback(env);
-					if (logging) console.log("call back");
-					if (logging) console.log(cb);
-					return cb;
-				} else {
-					return env;
-				}
-			}
-			if (after_cycle) after_cycle(env);
-			if (logging) console.log("is goal?:" + (go ? true : false));
-			if (logging) console.log(env);
-			window.requestAnimationFrame(frame);
-		},
-		exec = start
-			? () => {
-				console.log("func started");
-				window.requestAnimationFrame(frame);
-			}
-			: (() => {
-				window.requestAnimationFrame(frame);
-				return true;
-			})();
-	return { stop, exec };
-}
-
 export function forQuery(that, query, cbfn) {
 	return [...that.querySelectorAll(query)].forEach((_) => cbfn(_));
 }
@@ -43,8 +6,8 @@ export function setProps(that, props) {
 	Object.entries(props).forEach(([prop, val]) => that.style.setProperty(prop, val))
 }
 
-export function events(that, events, cb) {
-	events.split(" ").forEach((event) => that.addEventListener(event, cb))
+export function events(that, events, cb, options = {}) {
+	events.split(" ").forEach((event) => that.addEventListener(event, cb, options))
 }
 
 export function timeoutLimiter({ cooldown = 60, last } = {}) {
