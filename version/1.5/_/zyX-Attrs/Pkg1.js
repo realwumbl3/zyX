610a179a509a768390fb183a1bf50a9c0a5a3023
wumbl3 <wumbl3@gmail.com>
2024-04-28 23:05:27 -0400

    lol

--- version/1.5/_/zyX-Attrs/Pkg1.js
+++ version/1.5/_/zyX-Attrs/Pkg1.js
@@ -3,23 +3,17 @@ import { zyXFetchCSS } from "../zyX-Fetch.js";
 
 
 export function ClickPop({ node }) {
-    const func = (e) => {
+    function func(e) {
         const elementBounds = node.getBoundingClientRect();
         const xPer = elementBounds.width * ((e.clientX - elementBounds.x) / elementBounds.width);
         const yPer = elementBounds.height * ((e.clientY - elementBounds.y) / elementBounds.height);
         html`
 			<div class="zyx-expand_circle_ctn" this=ctn>
-				<div class="zyx-expand_circle" this=circle></div>
+				<div class="zyx-expand_circle" this=circle style="left:${xPer}px;top:${yPer}px"></div>
 			</div>
 		`.const()
             .appendTo(node)
-            .pass(_ => {
-                _.circle.style.left = xPer + "px";
-                _.circle.style.top = yPer + "px";
-                _.circle.addEventListener("animationend", function () {
-                    _.ctn.remove();
-                })
-            })
+            .pass(zyx => zyx.circle.addEventListener("animationend", _ => zyx.ctn.remove()))
     }
     node.addEventListener("pointerdown", func);
 }
@@ -29,22 +23,19 @@ export function Uplate({ node, data }) {
     node.active_template = "default";
     node.add = ({ template_name, template, carry_values } = {}) => {
         if (template_name in node.templates) return node;
-
         node.templates[template_name] = {
             fragments: {},
             values: {},
         };
-
         const active_template_data = node.templates[template_name];
         const prev_active = node.templates[node.active_template];
-
         if (carry_values && prev_active.values) {
             Object.assign(active_template_data.values, prev_active.values);
         }
         active_template_data.template = template;
         const matches = template.match(/{([a-z0-9]*?){(.*?)}}/gm);
         if (matches)
-            for (let var_template of matches) {
+            for (const var_template of matches) {
                 const var_data = /{([a-z0-9]*?){(.*?)}}/gm.exec(var_template);
                 const [fragment, var_name, default_val] = var_data;
                 if (default_val) active_template_data.values[var_name] = default_val;
@@ -61,8 +52,8 @@ export function Uplate({ node, data }) {
         const active_template_data = node.templates[node.active_template];
         let string_template = `${active_template_data.template}`; // clone string
         if (new_data) Object.assign(active_template_data.values, new_data);
-        for (const [var_tmp, getval] of Object.entries(active_template_data.fragments)) {
-            string_template = string_template.replace(var_tmp, getval());
+        for (const [target, get_val] of Object.entries(active_template_data.fragments)) {
+            string_template = string_template.replace(target, get_val());
         }
         node.innerHTML = string_template;
         return node;
